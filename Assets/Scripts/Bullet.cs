d2d5ad4d98e282b0d2c881ba080d499a68af221f
ThaliaDeShalit <thaliapenguin@gmail.com>
2014-12-29 13:42:53 +0200

    destroyBullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -48,11 +48,15 @@ public class Bullet : MonoBehaviour
 		//	Destroy (gameObject);
 		//}
 		// Otherwise if the player manages to shoot himself...
-		else if(col.gameObject.tag != "Player")
-		{
+		//else if(col.gameObject.tag != "Player")
+		//{
 			// Instantiate the explosion and destroy the rocket.
+		//	OnExplode();
+		//	Destroy (gameObject);
+		//}
+		else if (col.tag == "ground") {
+			Destroy(gameObject);
 			OnExplode();
-			Destroy (gameObject);
 		}
 	}
 }
