4a1ce3ec23f620f31bc46d17b7ac852742833499
Benom84 <menahem.ben@gmail.com>
2014-12-30 14:39:57 +0200

    Added MainMenu
    Still not working though

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class MainMenuScript : MonoBehaviour {
+
+	[HideInInspector]
+	public enum Button{Quit, Sound, Play, About}
+
+	public Button button;
+
+	void Start() {
+
+		if (button == Button.Sound)
+				if (!PlayerPrefs.HasKey ("Sound"))
+						PlayerPrefs.SetString ("Sound", "On");
+
+	}
+
+	void OnMouseDown() {
+		if (button == Button.Quit)
+			Application.Quit ();
+		else if (button == Button.Play)
+			Application.LoadLevel ("GrassLevel1");
+		else if (PlayerPrefs.GetString("Sound") == "On")
+			PlayerPrefs.SetString ("Sound", "Off");
+		else
+			PlayerPrefs.SetString ("Sound", "Off");
+	
+	}
+
+}
